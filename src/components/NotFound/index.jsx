f88513508e95cedd7900bf971f3df38634778f5d
Veselin <veselin.lk.petrov@gmail.com>
2019-04-19 17:46:39 +0300

    implemented NotFound component

---
+++ src/components/NotFound/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function NotFound() {
+    return (
+        <div
+            className="container"
+            style={{
+                width: '100%',
+                padding: '48px',
+                textAlign: 'center',
+                color: '#f00',
+            }}
+        >
+            Page not found
+        </div>
+    )
+}
