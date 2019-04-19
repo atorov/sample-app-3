2c65655a07facc8b0fd8ba07966602555e3dd23f
Veselin <veselin.lk.petrov@gmail.com>
2019-04-19 12:51:44 +0300

    updated index.ejs, index.jsx, and vscode settings

--- src/index.jsx
+++ src/index.jsx
@@ -5,33 +5,31 @@ import $ from 'jquery'
 
 import App from './components/App'
 
-setTimeout(() => {
-    const appRootElement = document.querySelector('#app-root')
-    const buildDate = appRootElement.dataset.buildDate
+const appRootElement = document.querySelector('#app-root')
+const buildDate = appRootElement.dataset.buildDate
 
-    function $remove() {
-        setTimeout(() => $(this).remove(), 5550)
-    }
+function $remove() {
+    setTimeout(() => $(this).remove(), 5550)
+}
 
-    $('#init-loader-wrapper')
-        .css('pointer-events', 'none')
-        .fadeTo(1550, 0, $remove)
+$('#init-loader-wrapper')
+    .css('pointer-events', 'none')
+    .fadeTo(1550, 0, $remove)
 
-    console.log('::: ::: :::')
-    console.log('::: process.env.NODE_ENV (NODE_ENV):', process.env.NODE_ENV)
-    console.log('::: process.env.BABEL_ENV:', process.env.BABEL_ENV)
-    console.log('::: module.hot:', !!module.hot)
-    console.log('::: APP_NAME:', APP_NAME)
-    console.log('::: APP_VERSION:', APP_VERSION)
-    console.log('::: Build Date:', buildDate)
-    console.log('::: ::: :::')
+console.log('::: ::: :::')
+console.log('::: process.env.NODE_ENV (NODE_ENV):', process.env.NODE_ENV)
+console.log('::: process.env.BABEL_ENV:', process.env.BABEL_ENV)
+console.log('::: module.hot:', !!module.hot)
+console.log('::: APP_NAME:', APP_NAME)
+console.log('::: APP_VERSION:', APP_VERSION)
+console.log('::: Build Date:', buildDate)
+console.log('::: ::: :::')
 
-    render(
-        // <StrictMode>
-        <App />,
-        // </StrictMode>,
-        document.querySelector('#app-root'),
-    )
-}, 250) // TODO:
+render(
+    // <StrictMode>
+    <App />,
+    // </StrictMode>,
+    document.querySelector('#app-root'),
+)
 
 export default null
